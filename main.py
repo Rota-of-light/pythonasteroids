ed0838a0c6350fa96d7ccf133d8d95b9e831c2df
Rota-of-light <Jeremiah.C.Applebee@gmail.com>
2024-09-01 00:29:16 -0700

    Creation of asteroids and collision

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+import sys
 import pygame
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
 
 def main():
     pygame.init()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -14,13 +20,18 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
         for item in updatable:
             item.update(dt)
+        for item in asteroids:
+            if item.collision(player):
+                print("Game over!")
+                sys.exit()
+        screen.fill((0, 0, 0))
         for item in drawable:
             item.draw(screen)
         pygame.display.flip()
